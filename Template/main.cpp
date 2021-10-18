d47836ee0d5787b970fe378705abb23bbe5a6d06
tjkfly <510991180@qq.com>
2021-10-18 22:20:54 +0800

    stl

--- Template/main.cpp
+++ Template/main.cpp
@@ -1,38 +1,90 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
-// 函数模板
-template <typename T>
-T my_max(T a, T b)
-{
-    return a > b?a:b;
-}
 
-template <typename T>
-T my_max(T a[], int n)
+
+namespace TJK
 {
-    T temp;
-    if(n <= 0)
+    template<typename T>
+    T max(T a,T b)
     {
-        return 0;
+        return a > b ? a:b;
     }
-    for (int i = 1;i < n;i++)
+    //重载函数模板
+    template<typename T>
+    T max(T a[],int n)
     {
-        temp = temp > a[i]?temp:a[i];
+        T temp;
+        int i;
+        temp = a[0];
+        for (i = 1;i<n;i++)
+        {
+            temp = temp > a[i]?temp:a[i];
+        }
+        return temp;
     }
-}
+    const int size = 10;
+    // 类模板
+    template <typename Atype>
+    class atype
+    {
+    public:
+        atype()
+        {
 
-// 类模板
-template<typename AType> class atype
-{
+            int i;
 
+            for(i = 0;i<size;i++)
+            {
+                array[i] = i;
+            }
+        }
+        Atype &operator[](int n);
+    private:
+
+        Atype array[size];
+    };
+    template <typename Atype>
+    Atype &atype<Atype>::operator[](int n)
+    {
+        if(n < 0 || n >= size)
+        {
+            cout<<"下标超出范围"<<endl;
+            exit(1);
+        }
+        return array[n];
+    }
 };
 
-int main() {
-    int a = 0,b = 0;
-    cin>>a>>b;
-    cout<<my_max(a,b)<<endl;
-    int arr[5] = { 1,3, 4,7,8};
-    cout<<my_max(arr,5)<<endl;
+
+int main()
+{
+//    int a,b;
+//    cout<<"please input two integers to a,b"<<endl;
+//    cin>>a>>b;
+//    cout <<"max("<<a<<","<<b<<")="<<TJK::max(a,b)<<endl;
+//
+//    char c,d;
+//    cout<<"please input two char to and d"<<endl;
+//    cin >>c>>d;
+//    cout<<"max("<<c<<","<<d<<") = "<<TJK::max(c,d)<<endl;
+//
+//    float x,y;
+//    cout<<"please input two floats to x and y"<<endl;
+//    cin >>x>>y;
+//    cout<<"max("<<x<<","<<y<<")="<<TJK::max(x,y)<<endl;
+//    int arr[5] = {1,34,5,6,7};
+//    cout<<TJK::max(arr,5);
+
+    TJK::atype<int> intob;
+    TJK::atype<double> doubleob;
+    for (int i = 0;i < 10;i++)
+        intob[i] = i + 1;
+    for(int i = 0;i<10;i++)
+        doubleob[i] = i /2.f;
+    for(int i = 0;i<10;i++)
+    {
+        cout<<intob[i]<<" , "<<doubleob[i]<<endl;
+    }
     return 0;
 }
